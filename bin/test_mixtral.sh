e44e90f2a04c241813b93b5f3a146f38f672a50c
Juan Manuel Pérez <jmperez.85@gmail.com>
2024-03-18 12:20:46 -0300

    GPT-q results

--- bin/test_mixtral.sh
+++ bin/test_mixtral.sh
@@ -4,7 +4,7 @@
 for i in {1..5}
 do
     path="data/test_0${i}.csv"
-    out="data/test_0${i}_mixtral.csv"
+    out="data/runs/test_0${i}_mixtral.csv"
     echo "Predicting for test_0$i.csv -- output: $out"
     bash bin/predict_amd.sh --gptq4 --input $path --output_path $out --batch_size 8
 done
